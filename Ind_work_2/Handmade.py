57514058e7f10f5690bfe60104e0da1c988be750
Niksa7 <nikitasoroka228@gmail.com>
2023-12-22 15:42:16 +0300

    last

--- Ind_work_2/Handmade.py
+++ Ind_work_2/Handmade.py
@@ -1,29 +1,17 @@
 import cv2
 import numpy as np
 
-def contour_object(binary_mask):
-    # Получаем размеры изображения
-    height, width = binary_mask.shape
-
+def find_contours(binary_mask):
     # Создаем пустую матрицу для хранения результата
     result = np.zeros_like(binary_mask)
 
     # Проходим по каждому пикселю внутри изображения
-    for i in range(1, height - 1):
-        for j in range(1, width - 1):
+    for i in range(1, binary_mask.shape[0] - 1):
+        for j in range(1, binary_mask.shape[1] - 1):
             # Если текущий пиксель - белый
             if binary_mask[i, j] == 255:
                 # Проверяем вокруг текущего пикселя
-                neighbors = [
-                    binary_mask[i - 1, j - 1],
-                    binary_mask[i - 1, j],
-                    binary_mask[i - 1, j + 1],
-                    binary_mask[i, j - 1],
-                    binary_mask[i, j + 1],
-                    binary_mask[i + 1, j - 1],
-                    binary_mask[i + 1, j],
-                    binary_mask[i + 1, j + 1],
-                ]
+                neighbors = binary_mask[i - 1:i + 2, j - 1:j + 2].flatten()
 
                 # Если хотя бы один из соседей - черный, то текущий пиксель находится на границе
                 if 0 in neighbors:
@@ -31,33 +19,32 @@ def contour_object(binary_mask):
 
     return result
 
+def main():
+    # Загрузка изображения
+    image = cv2.imread('in/car_1.jpg')
 
-# Загрузка изображения
-image = cv2.imread('in/car_3.jpg')
+    # Преобразование изображения из BGR в HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-# Преобразование изображения из BGR в HSV 2 6 7 8 9 10
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Определение диапазона цвета
+    lower_purple = np.array([110, 50, 50])
+    upper_purple = np.array([160, 255, 255])
 
-# Определение диапазона цвета
-lower_purple = np.array([110, 50, 50])
-upper_purple = np.array([160, 255, 255])
+    # Создание маски с использованием цветового фильтра
+    mask = cv2.inRange(hsv, lower_purple, upper_purple)
 
-# Создание маски с использованием цветового фильтра
-mask = cv2.inRange(hsv, lower_purple, upper_purple)
+    # Применение операций морфологии для улучшения результатов
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-# Применение операций морфологии для улучшения результатов
-kernel = np.ones((5, 5), np.uint8)
-mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-contours = contour_object(mask)
-# Нахождение контуров на изображении
-#contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = find_contours(mask)
 
-# Отображение контуров на исходном изображении
-#cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+    # Отображение исходного и обработанного изображений
+    cv2.imshow('Original Image', image)
+    cv2.imshow('Contours', contours)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# Отображение исходного и обработанного изображений
-cv2.imshow('Original Image', image)
-cv2.imshow('Contours', contours)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
